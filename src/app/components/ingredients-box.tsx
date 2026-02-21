c8a8bb9986dd51f918cc371608c03d16c2631993
Oleksii Kireiko <a.kireyko@gmail.com>
2026-02-21 21:59:25 +0200

    added: recipe recalc1

--- src/app/components/ingredients-box.tsx
+++ src/app/components/ingredients-box.tsx
@@ -9,67 +9,36 @@ interface IngredientsBoxProps {
 
 export default function IngredientsBox({ ingredients }: IngredientsBoxProps) {
   const [isCalculate, setIsCalculate] = useState(false);
+  const [ingredientsForRecalc, setIngredientsForRecalc] = useState([]);
   const handlerIsCalculate = () => {
+    const originalIngredients = ingredients.map((item) => item.amount);
+    setIngredientsForRecalc(originalIngredients);
     setIsCalculate(!isCalculate);
-    localStorage.setItem("ingredients", JSON.stringify(ingredients));
+    // localStorage.setItem("ingredients", JSON.stringify(ingredients));
   };
 
-  const handlerChange = (e) => {
-    const id = e.target.dataset.id;
-    console.log(id);
+  const handlerChange = (i, value) => {
+    console.log(i, value);
   };
 
-  const handlerCalculate = (e) => {
-    const id = e.target.dataset.id;
-    const id2 = e.target.dataset.id;
-    console.log(id);
-  };
+  // const handlerCalculate = (e) => {
+  //   const id = e.target.dataset.id;
+
+  //   console.log(id);
+  // };
 
   return (
     <>
       <p>Ingredients:</p>
-      <div className="flex gap-6">
-        <div className="w-8 h-8 flex items-center justify-center overflow-hidden cursor-pointer">
-          <Image
-            onClick={handlerIsCalculate}
-            width={32}
-            height={32}
-            src="/calculator.svg"
-            alt="calculator"
-            title="Click here to open recalculate menu"
-          />
-        </div>
+      <p>original proportions:</p>
+
+      <div className="flex gap-4">
         <ul className="flex flex-col gap-2">
           {ingredients.map((item) => (
             <li
               key={item.ingredient_id}
               className="grid grid-cols-2 gap-x-2 gap-y-3"
             >
-              <div className="w-24">
-                {isCalculate && (
-                  <div
-                    className="flex gap-2 cursor-pointer"
-                    data-id={item.ingredient_id}
-                    onClick={handlerCalculate}
-                  >
-                    <input
-                      type="number"
-                      defaultValue={item.amount}
-                      className="border-amber-500 bg-blue-600 w-full h-full px-2 rounded-sm"
-                      disabled
-                    />
-                    <div className="w-6 h-6 flex items-center justify-center overflow-hidden cursor-pointer">
-                      <Image
-                        width={24}
-                        height={24}
-                        src="/calculator.svg"
-                        alt="calculator"
-                        title="Click here to change amount"
-                      />
-                    </div>
-                  </div>
-                )}
-              </div>
               <Ingredient
                 ingredient={item.ingredient}
                 amount={item.amount}
@@ -78,6 +47,40 @@ export default function IngredientsBox({ ingredients }: IngredientsBoxProps) {
             </li>
           ))}
         </ul>
+        <div className="flex gap-6">
+          <div className="w-8 h-8 flex items-center justify-center overflow-hidden cursor-pointer">
+            <Image
+              onClick={handlerIsCalculate}
+              width={32}
+              height={32}
+              src="/calculator.svg"
+              alt="calculator"
+              title="Click here to open recalculate menu"
+            />
+          </div>
+          <ul className="flex flex-col gap-2">
+            {ingredientsForRecalc.map((item, i) => (
+              <li key={i} className="grid grid-cols-2 gap-x-2 gap-y-3">
+                <div className="w-24">
+                  {isCalculate && (
+                    <div
+                      className="flex gap-2 cursor-pointer"
+                      // data-id={item}
+                      // onClick={handlerCalculate}
+                    >
+                      <input
+                        type="number"
+                        value={item}
+                        className="border-amber-500 bg-blue-600 w-full h-full px-2 rounded-sm"
+                        onChange={(e) => handlerChange(i, e.target.value)}
+                      />
+                    </div>
+                  )}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
